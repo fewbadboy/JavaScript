687666e5d5d24e4f1b7f795847b78112de7e9245
fewbadboy <renekton.king@qq.com>
2021-03-26 01:56:17 +0800

    preorderTraversal

--- Data structure and algorithm/Tree/BinaryTree.js
+++ Data structure and algorithm/Tree/BinaryTree.js
@@ -1,1 +1,26 @@
-// 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+// 前序遍历--递归
+var preorderTraversal = function(root) {
+    return root ? [root.val, ...preorderTraversal(root.left), ...preorderTraversal(root.right)] : []
+};
+// 前序遍历--迭代
+var preorderTraversal = function(root) {
+    let arr = [], res = []
+    root && arr.push(root)
+
+    while(arr.length > 0) {
+        let cur = arr.pop()
+        res.push(cur.val)
+
+        cur.right && arr.push(cur.right)
+        cur.left && arr.push(cur.left)
+    }
+    return res
+};
